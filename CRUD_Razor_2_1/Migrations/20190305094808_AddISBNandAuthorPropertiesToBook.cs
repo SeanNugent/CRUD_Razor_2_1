8e99e68ad62cf467a16edf37a0618fe49b857a3a
Jason IBISI <jasonibisi@gmail.com>
2019-03-05 11:11:04 +0100

    Create Get and Post Handlers

---
+++ CRUD_Razor_2_1/Migrations/20190305094808_AddISBNandAuthorPropertiesToBook.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CRUD_Razor_2_1.Migrations
+{
+    public partial class AddISBNandAuthorPropertiesToBook : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Author",
+                table: "Books",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ISBN",
+                table: "Books",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Author",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "ISBN",
+                table: "Books");
+        }
+    }
+}
